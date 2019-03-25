a7d4f230e85dbfdcdc4a4972e47d0402243e8e41
Miguel <miguelangel.garcia@salenda.es>
2019-03-25 22:36:43 +0100

    Adding Spring Boot Hello World example and Micronaut Graal Hello World example. Also, a script to run an app is added

---
+++ micronautgraalhelloworld/docker-build.sh
@@ -0,0 +1,5 @@
+docker build . -t micronautgraalhelloworld
+echo
+echo
+echo "To run the docker container execute:"
+echo "    $ docker run --network host micronautgraalhelloworld"
---
+++ startup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar springbootjavahelloworld/build/libs/springbootjavahelloworld-0.0.1-SNAPSHOT.jar  | grep 'Started SpringbootjavahelloworldApplication' > tmp.x
